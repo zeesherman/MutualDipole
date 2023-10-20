20c39224283d6c1a0efef6fbdde5a1daca4c973c
Zach Sherman <zachary.m.sherman@gmail.com>
2023-10-20 13:00:44 -0700

    Update hoomd syntax in example

--- examples/run.py
+++ examples/run.py
@@ -1,5 +1,7 @@
-from hoomd_script import *
-from hoomd_plugins import MutualDipole
+from hoomd import *
+import hoomd
+from hoomd import md
+from hoomd import MutualDipole
 
 import math
 import numpy as np
@@ -15,18 +17,12 @@ xi = 0.5  # Ewald splitting parameter
 fileprefix = "lambdainf_phi0.50"  # prefix for output files
 
 # Typical nondimensionalization
-mass = dt  # particle mass; this ensures overdamped dynamics if a two step integrator is used
 diameter = 2.  # particle diameter
 radius = 1.  # particle radius; sets the length scale to be the particle radius
 gamma = 1.  # particle drag coefficient; sets the time scale to be the diffusion time
 
-# Other quantities
-L = (4.*math.pi*N/(3.*phi))**(1./3.)  # box dimension
-
-# Parameters for initializing system on a simple cubic lattice
-m = int(np.ceil(N**(1./3.)))  # smallest latticle dimension that can hold all of the particles
-a = L/m  # distance between lattice sites
-lo = -L/2.  # used to center the lattice about the origin
+# Box dimension
+L = (4.*math.pi*N/(3.*phi))**(1./3.)
 
 # Create a snapshot of an empty system
 context.initialize()
@@ -35,28 +31,35 @@ snapshot = data.make_snapshot(N=N, box=data.boxdim(L=L), particle_types=['A'])
 # Initialize the system from the snapshot
 system = init.read_snapshot(snapshot)
 
+# Parameters for initializing system on a simple cubic lattice
+m = int(np.ceil(N**(1./3.)))  # smallest latticle dimension that can hold all of the particles
+a = L/m  # distance between lattice sites
+lo = -L/2.  # used to center the lattice about the origin
+
 # Initialize a simple cubic array of particles
 for p in system.particles:
     (i,j,k) = (p.tag % m, p.tag/m % m, p.tag/m**2  % m)
     p.position = (lo + i*a + a/2, lo + j*a + a/2, lo + k*a + a/2)
     p.type = 'A'
-    p.mass = mass
     p.diameter = diameter
 
-# In the Heyes-Melrose algorithm, hard sphere interactions takes on the form of a Hookean spring with a time step dependent spring constant.
-def hs_potential(r,rmin,rmax,coeff1,coeff2,coeff3):
-    V = coeff1/4.*(r - coeff2)**2.
-    F = -coeff1/2.*(r - coeff2) 
-    return (V, F)
+# Create neighbor list
+nl = md.nlist.cell()
+
+# Function for hard sphere potential
+def hs_potential(r, rmin, rmax, dt):
+    U = 1./(4.*dt)*(2. - r)**2.
+    F = 1./(2.*dt)*(2. - r)
+    return(U, F)
 
 # Create an interpolation table for the hard sphere interactions
-table_hs = pair.table(width=1000)
-table_hs.pair_coeff.set('A','A',func=hs_potential,rmin=0.,rmax=diameter,coeff=dict(coeff1=1/dt,coeff2=diameter,coeff3=0.))
+hs = hoomd.md.pair.table(width=1000, nlist=nl)
+hs.pair_coeff.set('A', 'A', func=hs_potential, rmin=0., rmax=2., coeff=dict(dt=dt))
 
 # Establish Brownian dynamics integrator.
 all = group.all()
-integrate.mode_standard(dt=dt)
-bd = integrate.brownian(group=all, T=0, seed=datetime.now().microsecond)  # T = 0 turns off Brownian motion
+md.integrate.mode_standard(dt=dt)
+bd = md.integrate.brownian(group=all, kT=0, seed=datetime.now().microsecond)  # T = 0 turns off Brownian motion
 bd.set_gamma('A', gamma=gamma)
 
 # Turn on the field
